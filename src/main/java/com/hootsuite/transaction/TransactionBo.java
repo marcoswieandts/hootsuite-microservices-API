10f5b160b3ed333ca1a684b2beb2e213fd255fb0
Marcos Wienandts <marcoswienandts@gmail.com>
2016-05-23 15:44:49 -0300

    first

---
+++ src/main/java/com/hootsuite/transaction/TransactionBo.java
@@ -0,0 +1,7 @@
+package com.hootsuite.transaction;
+ 
+public interface TransactionBo{
+ 
+	String save();
+ 
+}
---
+++ src/main/java/com/hootsuite/transaction/impl/TransactionBoImpl.java
@@ -0,0 +1,13 @@
+package com.hootsuite.transaction.impl;
+
+import com.hootsuite.transaction.TransactionBo;
+
+public class TransactionBoImpl implements TransactionBo {
+
+	public String save() {
+
+		return "GraphDB - method save - Success";
+
+	}
+
+}
